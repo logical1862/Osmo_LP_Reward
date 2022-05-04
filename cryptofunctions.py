7453d6f0d3fd6bb4e97cd6969fa12144809d1039
logical1862 <100737462+logical1862@users.noreply.github.com>
2022-05-04 14:45:49 -0500

    Osmo_LP_Reward V2
    added functions to retrieve osmo price history and volume history
    reworked matplotlib use to plot all fields as different axes on a subplot to visualize: reward data over time, vol history, and price history
    
    code re written to implement those functions
    
    future development will focus on general aesthetics, saving data to a file for future reference(appending that file if it already exists), and increasing data fields

--- cryptofunctions.py
+++ cryptofunctions.py
@@ -2,21 +2,21 @@ import matplotlib.pyplot as plt
 import requests
 import json
 import pandas as pd
-
 from requests import session
+import os
 
 
 
-class all_token_data:
-    def __init__(self, daily, total, gain, price, name) -> None:
-        self.daily = list(daily)
-        self.total = list(total)
-        self.gain = list(gain)
-        self.price = list(price)
-        self.name = list(name)
 
-
-
+class Osmo_data:
+    def __init__(self) -> None:
+        self.daily = list()
+        self.total = list()
+        self.gain = list()
+        self.currentprice = list()
+        self.name = list()
+        self.histprice = list()
+        self.apr = list()
 
 
 def price(name, api_key):
@@ -34,9 +34,9 @@ def price(name, api_key):
     session.headers.update(headers)
 
     response1 = session.get(url = url, params= params)
-    content = json.loads(response1.text)
 
     price = json.loads(response1.content)['data'][name]['quote']['USD']['price']
+    
     return float(price)
 
 
@@ -52,26 +52,82 @@ def total_reward(dict):
     return total
 
 
+def osmo_hist_price_APR():
+    url = f'https://api-osmosis.imperator.co/tokens/v2/historical/OSMO/chart?tf=60'
+    headers = {
+      'Accepts': 'application/json', 
+    }
+    session = requests.Session()
+    session.headers.update(headers)
+
+    response = session.get(url = url)
+    content = json.loads(response.content)
+    content = pd.DataFrame(content)
+  
+    content['time'] = pd.to_datetime(content['time'],unit='s')
+    return content
 
 
-def osmo_reward_line_plot(all_token_data: all_token_data):
+
+def osmo_plot(osmo_data: Osmo_data):
+
+
+# set plot for rewards history
+    a = 65
+    b = 0
+    num_tokens = len(osmo_data.name)
+    plt.figure, axs1 =  plt.subplot_mosaic(
+      '''
+      AF
+      BF
+      CF
+      DH
+      EH
+      ''',
+      figsize=(20,8)      
     
-    a = 0
-    num_tokens = len(all_token_data.name)
+    )
 
-    plt.figure, axs =  plt.subplots(num_tokens, sharex= True)
-    
-    for i in all_token_data.name:   
-       
-        axs[a].plot(all_token_data.daily[a])      
-        axs[a].set_title(all_token_data.name[a])
+    for i in osmo_data.name:   
+        #axs1[chr(a)].st   #style.use('seaborn-darkgrid')
         
+        axs1[chr(a)].plot(osmo_data.daily[b])      
+        axs1[chr(a)].set_title(i)
+        axs1[chr(a)].set_xlabel('Days of rewards for each token')
+        axs1[chr(a)].set_xlim([0, len(osmo_data.daily[b])])
+        max_value = max(osmo_data.daily[b])
+        max_value = max_value + (max_value * 0.5)
+        axs1[chr(a)].set_ylim([0, max_value])
         a = a + 1
-    plt.margins(y = 5, x= 5)
-    plt.figure.supylabel('Token rewarded per day')
-    plt.figure.supxlabel('Days since start')
-    plt.tight_layout()
-    plt.show()
+        b += 1
+    
+    plt.margins(y = 10, x= 10)
+
+    osmo_hist = osmo_hist_price_APR()
+    print()
+       
+    axs1[chr(a )].bar('time', 'volume', data=osmo_hist, color='blue', width=0.05, linewidth=0.005)      
+    axs1[chr(a )].set_title('OSMO Volume(in millions')
+    axs1[chr(a )].grid()
+
+
+    axs1[chr(a + 2)].plot('time', 'close', data=osmo_hist, color='red') 
+    axs1[chr(a + 2)].plot('time', 'open', data=osmo_hist, color='green')       
+    axs1[chr(a + 2)].set_title('OSMO (green = open/ red = close')
+    
+    plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=.2, 
+                    hspace=2)
+    
+    return
+
+
+
+##   COSMOS ##
+
 
 
 def cosmos_account(wallet):
@@ -103,32 +159,30 @@ def cosmos_history():
 
     return history_df
 
-def osmo_price_APR():
-  url = f'https://api-osmosis.imperator.co/tokens/v2/historical/OSMO/chart?tf=60'
-  headers = {
-    'Accepts': 'application/json', 
-  }
-  session = requests.Session()
-  session.headers.update(headers)
 
-  response = session.get(url = url)
-  content = json.loads(response.content)
-  content = pd.DataFrame(content)
-  
-  content['time'] = pd.to_datetime(content['time'],unit='s')
-  
-  plt.figure, axs =  plt.subplots(2)
-       
-  axs[0].plot('time', 'volume', data=content, color='blue')      
-  axs[0].set_title('OSMO Volume(in millions')
+def token_data(i, wallet, api_key):
+    i = str(i)
+    name = i[11:len(i) - 2]
+    print(f'checking {name}')
+    
+    url = f'https://api-osmosis-chain.imperator.co/lp/v1/rewards/historical/{wallet}/{name}'
+    
+    
+    rewards_response = requests.get(url = url)
+    try:
+      #get total/price of rewards
+      dict = json.loads(rewards_response.content)
+      
+      total = total_reward(dict)
+      
+      current_price = price(name = name, api_key = api_key)
+      
+    except:
+      print('error within response')
+      input()
+      os.exit()
 
-  axs[1].plot('time', 'close', data=content, color='red') 
-  axs[1].plot('time', 'open', data=content, color='green')       
-  axs[1].set_title('OSMO (green = open/ red = close')
-  
-  plt.tight_layout()
-  
-  plt.rcParams["figure.figsize"] = (10, 10)
-  plt.show()
-  
-  return 
+    gain = current_price * total
+
+    print(f"Total {name} coin reward (from lp): {total}\nAt current price: {current_price}\ncurrent profit from lp rewards in {name}: {gain}\n")
+    return [name, dict, gain, current_price, total]
--- osmo_lp_main.py
+++ osmo_lp_main.py
@@ -1,110 +1,70 @@
+import cryptofunctions as crypto
 import requests
 import json
-import os
-import cryptofunctions as crypto
+import matplotlib.pyplot as plt
 
 
 
 
-#get coinmarketcap api key to pass to price() params
+# set all class data as empty lists
+daily_token = list()#daily
+total = list()#total of daily rewards
+current_price = list()# current trade price 
+gain = list()# total at current price
+name = list()# tokens rewarded in osmo wallet
+hist_price = list()
+apr = list()
+
+
+# get user input data
+
 api_key = input('CoinMarketCap API key?: \n')
-
-#get coin name
 wallet = input('OSMO wallet address?: \n')
 
-
+# get user results
 url_token_list = f'https://api-osmosis-chain.imperator.co/lp/v1/rewards/token/{wallet}'
-
 token_list_response = requests.get(url= url_token_list)
 tokens = json.loads(token_list_response.content)
 
 print(f'Coins rewarded from LP: {tokens}')
 
-#get rewards
-
-
-x_axis_list = list()#daily
-total = list()#total
-price = list()
-gain = list()
-name = list()
-
-
-#initialize class containing all data lists
-all_data = crypto.all_token_data(daily=x_axis_list, total=total, gain=gain, price= price, name= name)
-
+#initialize class (users wallet info)
+osmo_data = crypto.Osmo_data()
 for i in tokens:
+    #print(i, '\n\n')
    
-    i = str(i)
-    name = i[11:len(i) - 2]
-    print(f'checking {name}')
+    all_data = crypto.token_data(i=i, wallet=wallet, api_key=api_key)
     
-    url = f'https://api-osmosis-chain.imperator.co/lp/v1/rewards/historical/{wallet}/{name}'
+    # get name
+    name = all_data[0]
     
+    # get daily
+    daily_list = (all_data[1][:]) 
+    daily_len = len(all_data[1][:])
     
-    rewards_response = requests.get(url = url)
-    try:
-      #get total/price of rewards
-      dict = json.loads(rewards_response.content)
-      total = crypto.total_reward(dict)
-      price = crypto.price(name = name, api_key = api_key)
+    for i in daily_list:
+      daily_token.append(i['amount'])
+    daily_token.reverse()
 
-    except:
-      print('error within response')
+    # get gain
+    gain = all_data[2]
 
-    gain = price * total
+    # get currently listed trade price
+    current_price = all_data[3]
 
-    print(f"Total {name} coin reward (from lp): {total}\nAt current price: {price}\ncurrent profit from lp rewards in {name}: {gain}\n")
+    # get total coin rewarded
+    total = all_data[4]
 
-    #######START OF GRAPH CODE
+    # each tokens(i) data added to relevant class list
+    osmo_data.daily.append(daily_token)
+    osmo_data.gain.append(gain)
+    osmo_data.name.append(name)
+    osmo_data.currentprice.append(current_price)
+    osmo_data.total.append(total)
 
-    chart_len = len(dict)
-    
-    x_axis_list = list()
-    y_axis_list = list()
-
-    
-    #x axis is each reward per day
-    for i in range(chart_len - 1):
-      x_axis_list.append(dict[i]['amount'])
-
-    x_axis_list.reverse()
-
-  
-    #each tokens(i) data added to relevant class list
-    all_data.daily.append(x_axis_list)
-    all_data.gain.append(gain)
-    all_data.name.append(name)
-    all_data.price.append(price)
-    all_data.total.append(total)
-    
-
-crypto.osmo_reward_line_plot(all_data)
+    daily_token = []
 
 
+crypto.osmo_plot(osmo_data=osmo_data)
 
-
-
-
-
-
-
-
-
-###possible dev        
-
-#make file with data (possibly with a pandas Dataframe)
-#    a = 0
-#    cwd = os.getcwd()
-#    fileType = '\\osmolist.txt'
-#    cwd = cwd + fileType
-
-#open and write file with contents of dict
-#with open(cwd, 'w') as f:
-    #
-
-      #
-
-      #f.write()
-      #f.write('\n')
-    #f.write(f"Total {name} coin reward (from lp): {total}\nAt current price: {price}\ncurrent profit from lp rewards in {name}: {gain}\n")
+plt.show()
